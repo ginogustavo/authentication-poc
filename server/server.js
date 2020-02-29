74651d062784ae1494ad3e26676472cf665141c2
Gino Gustavo <ing.gino@gmail.com>
2020-02-29 10:51:15 -0600

    Adding Generation unique strings

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 //npm modules
 const express = require('express');
+const uuid = require('uuid/v4');
 
 //create the server
 const app = express();
@@ -7,7 +8,10 @@ const app = express();
 //creates a home page route at '/'
 app.get('/', (req,res)=>{
     console.log(req);
-    res.send('You hit the home page without restarting the server\n');
+    const uniqueId = uuid();
+    //string interpolation, requires ‘back-ticks’ instead of quote marks.
+    //These (``) and not these (‘’)
+    res.send(`Home Page. Unique id: ${uniqueId}\n`);
 });
 
 //tell the server what port to listen on
