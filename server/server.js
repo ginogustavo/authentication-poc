e2a79cb754a49f968be273d52c5d919148e529d8
Gino Gustavo <ing.gino@gmail.com>
2020-02-29 10:38:14 -0600

    Adding Home route

--- server/server.js
+++ server/server.js
@@ -3,6 +3,14 @@ const express = require('express');
 
 //create the server
 const app = express();
+
+//creates a home page route at '/'
+app.get('/', (req,res)=>{
+    console.log(req);
+    res.send('You hit the home page without restarting the server\n');
+});
+
+//tell the server what port to listen on
 app.listen(3000, ()=> {
     console.log("Listen on localhost:3000");
 });
